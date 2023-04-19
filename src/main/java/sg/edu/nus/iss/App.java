87b786200500a7a6aeea7b6d7b9a5be534028736
Nigel Tay <82434359+nigel-tay@users.noreply.github.com>
2023-04-19 14:23:53 +0800

    create employee and csvwrite classes

--- src/main/java/sg/edu/nus/iss/App.java
+++ src/main/java/sg/edu/nus/iss/App.java
@@ -1,13 +1,34 @@
 package sg.edu.nus.iss;
 
+import java.io.File;
+import java.io.IOException;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException
     {
-        System.out.println( "Hello World!" );
+        String dirPath = args[0];
+        String fileName = args[1];
+        String dirPathFileName = dirPath + File.separator + fileName;
+
+        File newDirectory = new File(dirPath);
+        if (!newDirectory.exists()) {
+            newDirectory.mkdir();
+        }
+        else {
+            System.out.println("Path already exists.");
+        }
+
+        File newFile = new File(dirPathFileName);
+        if (!newFile.exists()) {
+            newFile.createNewFile();
+        }
+        else {
+            System.out.println("File exists.");
+        }
     }
 }
---
+++ src/main/java/sg/edu/nus/iss/CSVWriter.java
@@ -0,0 +1,60 @@
+package sg.edu.nus.iss;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CSVWriter {
+
+    // We can call static final variable via dot notation
+    public static final String COMMA_DELIMETER = ",";
+    public static final String NEWLINE_SEPARATOR = "\n";
+    public static final String FILE_HEADER = "staffNo,fullName,department,role,emailAddress,salary";
+
+    public List<Employee> employees = null;
+
+    public List<Employee> generateEmployees() {
+        employees = new ArrayList<>();
+
+        Employee emp1 = new Employee("1234", "bryan", "CRM", "HR", "bryan@visa.com", 10000);
+        Employee emp2 = new Employee("1235", "john", "CRM", "HR", "john@visa.com", 10000);
+        Employee emp3 = new Employee("1236", "sam", "CRM", "HR", "bryan@visa.com", 10000);
+        
+        employees.add(emp1);
+        employees.add(emp2);
+        employees.add(emp3);
+
+        return employees;
+    }
+
+    public void writeToCSV(List<Employee> employees, String fileName) throws IOException {
+        FileWriter fileWriter = new FileWriter(fileName);
+
+        // Write FILE_HEADER into specified file
+        BufferedWriter bw = new BufferedWriter(fileWriter);
+        bw.append(FILE_HEADER); // Appends eadch individual character in the string
+        
+        // Iterate through employees to write each employee record to file
+        for (Employee individuaEmployee: employees) {
+            bw.append(individuaEmployee.getStaffNo());
+            bw.append(COMMA_DELIMETER);
+            bw.append(individuaEmployee.getFullName());
+            bw.append(COMMA_DELIMETER);
+            bw.append(individuaEmployee.getDepartment());
+            bw.append(COMMA_DELIMETER);
+            bw.append(individuaEmployee.getRole());
+            bw.append(COMMA_DELIMETER);
+            bw.append(individuaEmployee.getEmailAddress());
+            bw.append(COMMA_DELIMETER);
+            bw.append(String.valueOf(individuaEmployee.getSalary()));
+            bw.append(NEWLINE_SEPARATOR);
+
+        }
+        // Flush and close fileWriter
+        bw.flush();
+        bw.close();
+        fileWriter.close();
+    }
+}
